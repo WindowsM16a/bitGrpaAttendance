dd77b428f8cd7c86a759992facd6df57a3abc91b
WindowsM16a <wuvershayne@gmail.com>
2025-05-03 14:32:12 +0000

    user auth

--- src/auth.js
+++ src/auth.js
@@ -57,6 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 			return emailRegex.test(email);
 		}
+		console.log(dom.emailV);
 		console.log(isValidEmail(dom.emailV));
 
 		// signInWithEmailAndPassword(auth, dom.emailV, dom.passwordV).then(
