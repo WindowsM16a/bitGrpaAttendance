36a2c9cd6766484935c4be0237c7d3c34c63c677
WindowsM16a <wuvershayne@gmail.com>
2025-05-03 13:01:44 +0000

    testing

---
+++ api/sendSms.js
@@ -0,0 +1,21 @@
+import twilio from "twilio";
+
+export default async function handler(req, res) {
+	if (req.method !== "POST") return res.status(405).end();
+
+	const { to, body } = req.body;
+
+	const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+
+	try {
+		const message = await client.messages.create({
+			body,
+			from: process.env.TWILIO_PHONE,
+			to,
+		});
+
+		res.status(200).json({ sid: message.sid });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+}
